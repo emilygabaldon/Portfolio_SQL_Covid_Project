99e1b4fb7d4c6da53386685ea0e6b7697e275da2
Emily Gabaldon <137608274+emilygabaldon@users.noreply.github.com>
2023-06-25 13:26:08 +0200

    SQLCovidProject.sql

--- SQLCovidProject.sql
+++ SQLCovidProject.sql
@@ -1,6 +1,11 @@
+--(EN)  English
+--(DEU) Deutsch
+--(ES)  EspaÃ±ol
+
+
 --(EN)  Checking Tables Information
---(DEU) Tabelleninformationen Prüfung
---(ES)  Comprobación de la información en las tablas
+--(DEU) Tabelleninformationen PrÃ¼fung
+--(ES)  ComprobaciÃ³n de la informaciÃ³n en las tablas
 SELECT *
 From Covid_Portfolio_Project..CovidDeaths
 order by 3,4
@@ -11,81 +16,72 @@ order by 3,4
 
 --(EN)  Selecting specific data from: CovidDeaths
 --(DEU) Auswahl spezifischer Daten aus: CovidDeaths
---(ES)  Selección de datos específicos de: CovidDeaths
+--(ES)  SelecciÃ³n de datos especÃ­ficos de: CovidDeaths
 SELECT location, date, total_cases, new_cases, total_deaths, population
 From Covid_Portfolio_Project..CovidDeaths
 order by 1,2
 
 --(EN)  Looking at the total cases vs Total deaths(likelihood of dying if you contractc Covid in Germany)
---(DEU) Gesamtzahl der Fälle im Vergleich zur Gesamtzahl der Todesfälle (die Wahrscheinlichkeit zu sterben, wenn man sich in Deutschland mit Covid infiziert)
+--(DEU) Gesamtzahl der FÃ¤lle im Vergleich zur Gesamtzahl der TodesfÃ¤lle (die Wahrscheinlichkeit zu sterben, wenn man sich in Deutschland mit Covid infiziert)
 --(ES)  Observando el total de casos frente al total de muertes (probabilidad de morir si contraes Covid en Alemania)
 SELECT location, date, total_cases, total_deaths,(total_deaths/total_cases)*100 as DeathPercentage
 From Covid_Portfolio_Project..CovidDeaths
 WHERE location='Germany'
 
---
 --(EN)  Looking at the total cases versus the population. What percentage of the population got covid?
---(DEU) Gesamtzahl der Fälle im Vergleich zur Bevölkerung. Wie viel Prozent der Bevölkerung hat Covid?
---(ES)  Observando el total de casos frente a la población. ¿Qué porcentaje de la población se contagió?
+--(DEU) Gesamtzahl der FÃ¤lle im Vergleich zur BevÃ¶lkerung. Wie viel Prozent der BevÃ¶lkerung hat Covid?
+--(ES)  Observando el total de casos frente a la poblaciÃ³n. Â¿QuÃ© porcentaje de la poblaciÃ³n se contagiÃ³?
 SELECT location, date, total_cases, Population, total_deaths,(total_cases/population)*100 as CasePercentage
 From Covid_Portfolio_Project..CovidDeaths
 WHERE location='Germany'
 order by 2
 
-
 --(EN)  What country has the highest infection rate compared to population?
---(DEU) Welches Land hat die höchste Infektionsrate im Verhältnis zur Bevölkerung?
---(ES)  ¿Qué país tiene la mayor tasa de infección en comparación con su población?
+--(DEU) Welches Land hat die hÃ¶chste Infektionsrate im VerhÃ¤ltnis zur BevÃ¶lkerung?
+--(ES)  Â¿QuÃ© paÃ­s tiene la mayor tasa de infecciÃ³n en comparaciÃ³n con su poblaciÃ³n?
 SELECT location, Max(total_cases) as HighestInfectionCount, Population,Max((total_cases/population))*100 as CasePercentage
 From Covid_Portfolio_Project..CovidDeaths
 Group by Location, population
 order by CasePercentage desc
 
-
 --(EN)  Showing the countries with the highests death count per population
---(DEU) Länder mit den meisten Todesfällen pro Bevölkerung
---(ES)  Países con mayor número de muertes por habitante
+--(DEU) LÃ¤nder mit den meisten TodesfÃ¤llen pro BevÃ¶lkerung
+--(ES)  PaÃ­ses con mayor nÃºmero de muertes por habitante
 SELECT location, Max(cast(total_deaths as int)) as TotalDeathCount
 FROM Covid_Portfolio_Project..CovidDeaths
 Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
-
 --(EN)  Breaking the information out by continent
---(DEU) Aufschlüsselung der Informationen nach Kontinenten
---(ES)  Desglose de la información por continentes
+--(DEU) AufschlÃ¼sselung der Informationen nach Kontinenten
+--(ES)  Desglose de la informaciÃ³n por continentes
 SELECT continent, Max(cast(total_deaths as int)) as TotalDeathCount
 FROM Covid_Portfolio_Project..CovidDeaths
 Where continent is not null
 Group by continent
 order by TotalDeathCount desc
 
-
 --(EN)  Global numbers per day
 --(DEU) Globale Zahlen pro Tag
---(ES)  Cifras globales por día
-
+--(ES)  Cifras globales por dÃ­a
 SELECT date, SUM(new_cases) as total_cases_world, SUM(cast(new_deaths as int)) as total_deaths_world, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as WorldDeathPercentage
 From Covid_Portfolio_Project..CovidDeaths
 where continent is not null
 group By date
 order by 1,2
 
-
 --(EN)  Global numbers in total
 --(DEU) Globale Zahlen insgesamt
 --(ES)  Cifras globales en total
-
 SELECT SUM(new_cases) as total_cases_world, SUM(cast(new_deaths as int)) as total_deaths_world, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as WorldDeathPercentage
 From Covid_Portfolio_Project..CovidDeaths
 where continent is not null
 order by 1,2
 
-
 --(EN)  Joining Tables
---(DEU) Tabellen verknüpfen
---(ES)  Unión de tablas
+--(DEU) Tabellen verknÃ¼pfen
+--(ES)  UniÃ³n de tablas
 Select *
 from Covid_Portfolio_Project..CovidDeaths dea
 join Covid_Portfolio_Project..CovidVaccinations vac
@@ -94,9 +90,8 @@ join Covid_Portfolio_Project..CovidVaccinations vac
 order by 1,2
 
 --(EN)  Looking at total Population versus Vaccinations
---(DEU) Betrachtung der Gesamtbevölkerung im Vergleich zu den Impfungen
---(ES)  Población total y vacunaciones
-
+--(DEU) Betrachtung der GesamtbevÃ¶lkerung im Vergleich zu den Impfungen
+--(ES)  PoblaciÃ³n total y vacunaciones
 
 --CTE
 With PopvsVac (Continent, Location, Date, Population, New_vaccinations, RollingPeopleVaccinated)
